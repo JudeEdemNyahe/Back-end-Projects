5428504659d26e05f3bb5ee8d3ded05317cb8162
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-07-02 22:20:22 +0000

    Delete Weekly-Dessert directory

--- Weekly-Dessert/cafeteria/weeklydessert/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# Import your Week and Choice models below:
-from .models import Week,Choice
-
-# Register your Week and Choice models below:
-admin.site.register(Week)
-admin.site.register(Choice)
